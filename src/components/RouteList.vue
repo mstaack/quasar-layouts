411ce7d13f7ab28801c9756f2bbbb2ad66339c09
Tomer Shalev <shalev.tomer@gmail.com>
2019-02-21 11:42:45 +0200

    Create separate RouteList component

---
+++ src/components/RouteList.vue
@@ -0,0 +1,42 @@
+<template>
+  <q-list
+    bordered
+  >
+    <q-item
+      v-for="(name, index) in routeNames"
+      :key="`route_${index}`"
+      clickable
+      :to="name"
+    >
+      <q-item-section>
+        <q-item-label>
+          {{ name }} layout
+        </q-item-label>
+      </q-item-section>
+    </q-item>
+  </q-list>
+</template>
+
+<script>
+
+export default {
+  data () {
+    return {}
+  },
+  props: {
+    routes: {
+      type: Array,
+      required: true
+    }
+  },
+  computed: {
+    routeNames () {
+      return this.routes
+        .map(x => x.children)
+        .map(children => children && children.map(child => child.name))
+        .flat()
+        .filter(x => x)
+    }
+  }
+}
+</script>
